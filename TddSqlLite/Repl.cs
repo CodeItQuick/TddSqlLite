3eed1d9c5dd855e3617d645be0fabf2ce2408b3a
CodeItQuick <evanontario@hotmail.com>
2023-12-23 15:03:35 -0400

    insert and create statements made

--- TddSqlLite/Repl.cs
+++ TddSqlLite/Repl.cs
@@ -7,6 +7,18 @@ public class Repl
     private readonly IConsoleWriteLineWrapper _writeLine;
     private readonly IConsoleInputWrapper _consoleInputWrapper;
     private readonly Stack<string> _commands = new();
+    private enum META_COMMANDS
+    {
+        EXIT,
+        UNRECOGNIZED_COMMAND
+    }
+
+    private enum STATEMENTS
+    {
+        CREATE,
+        INSERT
+    };
+
 
     public Repl(IConsoleWriteLineWrapper writeLine)
     {
@@ -31,15 +43,51 @@ public class Repl
             _writeLine.Print("sqlite> ");
             var currentCommandStack = _consoleInputWrapper.RetrieveCommands();
             var peek = currentCommandStack.TryPeek(out var command);
-            var validCommands = new List<string>();
-            if (command is "" or ".exit")
+            if (command.StartsWith("."))
             {
-                currentCommandStack.Pop();
-            } else if (peek && !validCommands.Contains(command ?? ""))
+                var metaCommand = MetaCommands(command, currentCommandStack);
+                if (metaCommand == META_COMMANDS.EXIT)
+                {
+                    currentCommandStack.Pop();
+                    continue;
+                }
+
+                if (metaCommand == META_COMMANDS.UNRECOGNIZED_COMMAND)
+                {
+                    _writeLine.Print($"Unrecognized command '{command}'.");
+                    continue;
+                }
+            }
+
+            var firstCommand = command.Split(" ").FirstOrDefault();
+            var tryParseStatement = Enum.TryParse<STATEMENTS>(firstCommand, out var statement);
+            if (tryParseStatement)
+            {
+                if (statement == STATEMENTS.CREATE)
+                {
+                    
+                }
+
+                if (statement == STATEMENTS.INSERT)
+                {
+                    
+                }
+            }
+            else
             {
                 _writeLine.Print($"Unrecognized command '{command}'.");
             }
             _consoleInputWrapper.WaitForInput();
         } while (_commands.Count > 0);
+    }
+
+    private META_COMMANDS MetaCommands(string command, Stack<string> currentCommandStack)
+    {
+        if (command is ".exit")
+        {
+            return META_COMMANDS.EXIT;
+        }
+
+        return META_COMMANDS.UNRECOGNIZED_COMMAND;
     }
 }
--- Tests/Repl.cs
+++ Tests/Repl.cs
@@ -4,6 +4,11 @@ namespace Tests;
 
 public class Repl
 {
+    private const string IntroText = "SQLite version 3.16.0 2016-11-04 19:09:39" +
+                                     "Enter \".help\" for usage hints.\nConnected to a transient " +
+                                     "in-memory database.\nUse \".open FILENAME\" to reopen on a persistent " +
+                                     "database.";
+
     [Fact]
     public void MainProducesTextOutput()
     {
@@ -21,30 +26,53 @@ public class Repl
                      "database.", writeLineWrapper.RetrieveMessage());
     }
     [Fact]
-    public void CanStartReplWithCommands()
+    public void CanStartReplWithCREATECommands()
     {
         var writeLineWrapper = new FakeConsoleWriteLineWrapper();
         var commands = new Stack<string>();
-        commands.Push(""); // exit
-        commands.Push("create table users (id int, username varchar(255), email varchar(255));");
+        commands.Push(".exit"); // exit
+        commands.Push("CREATE table users (id int, username varchar(255), email varchar(255));");
         var repl = new TddSqlLite.Repl(writeLineWrapper, new FakeConsoleInputWrapper(commands));
 
         repl.Start();
 
         var retrieveMessage = writeLineWrapper.RetrieveMessage();
-        Assert.Contains("SQLite version 3.16.0 2016-11-04 19:09:39" +
-                        "Enter \".help\" for usage hints.\nConnected to a transient " +
-                        "in-memory database.\nUse \".open FILENAME\" to reopen on a persistent " +
-                        "database.", retrieveMessage);
-        Assert.Contains("sqlite> ", retrieveMessage);
+        Assert.Contains(IntroText, retrieveMessage);
         Assert.Equal(3, retrieveMessage.Count);
+        Assert.Equivalent(new List<string>()
+        {
+            "sqlite> ", // enter .exit
+            "sqlite> ", // enter create statement
+            IntroText // intro text
+        }, retrieveMessage.ToList());
+    } 
+    [Fact]
+    public void CanStartReplWithInsertCommands()
+    {
+        var writeLineWrapper = new FakeConsoleWriteLineWrapper();
+        var commands = new Stack<string>();
+        commands.Push(".exit"); // exit
+        commands.Push("INSERT VALUES INTO users ('test', 'test@user.com');");
+        var repl = new TddSqlLite.Repl(writeLineWrapper, new FakeConsoleInputWrapper(commands));
+
+        repl.Start();
+
+        var retrieveMessage = writeLineWrapper.RetrieveMessage();
+        Assert.Contains(IntroText, retrieveMessage);
+        Assert.Equal(3, retrieveMessage.Count);
+        Assert.Equivalent(new List<string>()
+        {
+            "sqlite> ", // enter .exit
+            "sqlite> ", // enter create statement
+            IntroText // intro text
+        }, retrieveMessage.ToList());
     }
     [Fact]
     public void CanRecognizeWhenCommandInvalid()
     {
         var writeLineWrapper = new FakeConsoleWriteLineWrapper();
         var commands = new Stack<string>();
-        commands.Push(""); // exit
+        commands.Push(".exit"); // exit
         commands.Push("invalid command");
         var repl = new TddSqlLite.Repl(writeLineWrapper, new FakeConsoleInputWrapper(commands));
 
